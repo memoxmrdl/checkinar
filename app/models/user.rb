2c36f1b32b635dc53b76daceb881f2bab6ec2c7f
memoxmrdl <jmemox@gmail.com>
2019-08-27 11:01:21 -0500

    Add RoleModel for sets roles to users, Add Haml-rails for manage views with haml and add tests for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  include RoleModel
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  roles_attribute :roles_mask
+  roles :owner, :leader, :attender
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'landing_page#show'
+
+  root "landing_page#show"
 end
