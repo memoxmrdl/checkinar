72b8d5a4e9e6cf3b693680330b635c5add5cedcd
memoxmrdl <jmemox@gmail.com>
2019-09-11 14:28:12 -0500

    Remove OrganizationsController, simple_form gem, views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+    resources :organizations
+
+    root to: "users#index"
+  end
+
   devise_for :users
-  root 'landing_page#show'
-  resources :organizations
+
+  root "landing_page#show"
 end
--- db/migrate/20190827233438_create_organizations.rb
+++ db/migrate/20190827233438_create_organizations.rb
@@ -1,7 +1,8 @@
 class CreateOrganizations < ActiveRecord::Migration[6.0]
   def change
     create_table :organizations do |t|
-      t.string :name
+      t.string :name, null: false
+      t.boolean :active, default: true
 
       t.timestamps
     end
