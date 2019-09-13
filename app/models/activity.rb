570bbd0d804fa35334a66021da6fffaa8ea83d91
memoxmrdl <jmemox@gmail.com>
2019-09-13 13:57:23 -0500

    Users in activities

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Activity < ApplicationRecord
+  has_and_belongs_to_many :users
   belongs_to :organization
 
   validates :name, presence: true
--- test/models/activity_test.rb
+++ test/models/activity_test.rb
@@ -7,14 +7,28 @@ class ActivityTest < ActiveSupport::TestCase
     @subject = Activity.new
   end
 
-  def it_creates_valid
+  def test_it_creates_valid
     @subject.name = "Gym"
     @subject.organization = organizations(:michelada)
 
     assert @subject.save
   end
 
-  def it_creates_invalid
+  def test_it_creates_invalid
     assert_not @subject.save
+  end
+
+  def test_it_adds_a_leader
+    activity = activities(:traguitos)
+    activity.users << users(:leader)
+
+    assert activity.users.leader.any?
+  end
+
+  def test_it_adds_an_attender
+    activity = activities(:traguitos)
+    activity.users << users(:attender)
+
+    assert activity.users.attender.any?
   end
 end
