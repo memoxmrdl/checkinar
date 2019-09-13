59d4d2e6993fd0bf0e71d8decd08b43c207d85fa
memoxmrdl <jmemox@gmail.com>
2019-09-13 17:18:40 -0500

    Create routes for owner, leader and attender

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,33 @@ Rails.application.routes.draw do
     root to: "users#index"
   end
 
+  scope module: :api do
+    resources :activities
+    resources :attenders
+    resources :attendances
+  end
+
   devise_for :users
+
+  namespace :owner do
+    resources :activities do
+      resources :leaders, only: %i[index create destroy]
+    end
+  end
+
+  namespace :leader do
+    resources :activities, except: %i[destroy] do
+      resources :attenders, only: %i[index show create destroy] do
+        resources :attendances, only: %i[create edit update destroy]
+      end
+    end
+  end
+
+  namespace :attender do
+    resources :activities, only: %i[index show] do
+      resources :attendances, only: %i[create destroy]
+    end
+  end
 
   root "landing_page#show"
 end
