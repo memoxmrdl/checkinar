34730754af036be0c19d41d819b603029a076fe7
memoxmrdl <jmemox@gmail.com>
2019-09-18 12:54:02 -0500

    Review changes

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -12,7 +12,7 @@ class Activity < ApplicationRecord
             :duration, presence: true, if: Proc.new { |a| a.occurs_on_changed? && a.date? }
   validates :occurs_frequency,
             :start_at,
-            :duration, presence: true, if: Proc.new { |a| occurs_on_changed? && a.more_than_once? }
+            :duration, presence: true, if: Proc.new { |a| a.occurs_on_changed? && a.more_than_once? }
 
   i18n_enum_attribute :occurs_on
 
