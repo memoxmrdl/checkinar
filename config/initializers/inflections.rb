7d3ba94a3e35656de6d24c2a37de6a29652e191c
memoxmrdl <jmemox@gmail.com>
2019-09-18 12:12:08 -0500

    Configure inflections

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,16 +1,21 @@
-# Be sure to restart your server when you modify this file.
+# frozen_string_literal: true
 
-# Add new inflection rules using the following format. Inflections
-# are locale specific, and you may define rules for as many different
-# locales as you wish. All of these examples are active by default:
-# ActiveSupport::Inflector.inflections(:en) do |inflect|
-#   inflect.plural /^(ox)$/i, '\1en'
-#   inflect.singular /^(ox)en/i, '\1'
-#   inflect.irregular 'person', 'people'
-#   inflect.uncountable %w( fish sheep )
-# end
+# Grabbed from https://github.com/davidcelis/inflections/blob/master/lib/inflections/es.rb
 
-# These inflection rules are supported but not enabled by default:
-# ActiveSupport::Inflector.inflections(:en) do |inflect|
-#   inflect.acronym 'RESTful'
-# end
+ActiveSupport::Inflector.inflections(:en) do |inflect|
+  inflect.acronym "API"
+end
+
+ActiveSupport::Inflector.inflections(:es) do |inflect|
+  inflect.clear :all
+
+  inflect.plural /([^djlnrs])([A-Z]|_|$)/, '\1s\2'
+  inflect.plural /([djlnrs])([A-Z]|_|$)/, '\1es\2'
+  inflect.plural /(.*)z([A-Z]|_|$)$/i, '\1ces\2'
+
+  inflect.singular /([^djlnrs])s([A-Z]|_|$)/, '\1\2'
+  inflect.singular /([djlnrs])es([A-Z]|_|$)/, '\1\2'
+  inflect.singular /(.*)ces([A-Z]|_|$)$/i, '\1z\2'
+
+  inflect.irregular("el", "los")
+end
