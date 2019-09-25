61b7773331895327821c2a1b983fa70817238c5e
memoxmrdl <jmemox@gmail.com>
2019-09-25 11:35:22 -0500

    Change description tests

--- test/lib/point_inside_radius_test.rb
+++ test/lib/point_inside_radius_test.rb
@@ -10,14 +10,14 @@ class PointInsideRadiusTest < ActiveSupport::TestCase
     @radius_meters = 25
   end
 
-  def test_it_insides_current_location_at_michelada
+  def test_current_location_is_inside_at_michelada
     # https://www.google.com/maps/search/michelada/@19.266542,-103.718041,19z
     current_location = { latitude: 19.266542, longitude: -103.718041 }
 
     assert @subject.new(current_location, @michelada_location, @radius_meters).is_inside?
   end
 
-  def test_it_wont_inside_at_michelada_because_im_at_minisuper_golfito
+  def test_iam_not_inside_at_michelada
     # https://www.google.com/maps/place/MIinisuper+El+Golfito/@19.2666211,-103.7172217,20.6z/data=!4m8!1m2!2m1!1smichelada!3m4!1s0x84255ac5fc475c67:0xee378f4e77a4fcf5!8m2!3d19.2668934!4d-103.7170067
     minisuper_golfito_location = { latitude: 19.2666211, longitude: -103.7172217 }
 
