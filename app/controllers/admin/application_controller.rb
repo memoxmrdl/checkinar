998127625684dca7b539c304fba5345f1c0dc34a
memoxmrdl <jmemox@gmail.com>
2019-09-26 02:00:11 -0500

    Configure app for Heroku

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -8,11 +8,7 @@
 # you're free to overwrite the RESTful controller actions.
 module Admin
   class ApplicationController < Administrate::ApplicationController
-    before_action :authenticate_admin!
-
-    def authenticate_admin!
-      (user_signed_in? && current_user.is_owner?) || redirect_to(root_path, alert: I18n.t(".forbidden_access"))
-    end
+    http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"], if: -> { Rails.env.production? }
 
     # Override this value to specify the number of elements to display at a time
     # on index pages. Defaults to 20.
