f173264e613a14534913a572498270aca329a644
Miguel <miguel.urbina15.1997@gmail.com>
2019-09-26 19:56:21 -0500

    Add design to landing page

--- app/javascript/application/javascripts/font-awesome.js
+++ app/javascript/application/javascripts/font-awesome.js
@@ -1,14 +1,18 @@
 import fontawesome from '@fortawesome/fontawesome';
-import faSearch from '@fortawesome/fontawesome-free-solid/faSearch';
 import faUpload from '@fortawesome/fontawesome-free-solid/faUpload';
-import faMinusCircle from '@fortawesome/fontawesome-free-solid/faMinusCircle';
-import faCheck from '@fortawesome/fontawesome-free-solid/faCheck';
+import faMarkerAlt from '@fortawesome/fontawesome-free-solid/faMapMarkerAlt';
+import faUsers from '@fortawesome/fontawesome-free-solid/faUsers';
+import faSortNumeric from '@fortawesome/fontawesome-free-solid/faSortNumericUp';
+import faMobile from '@fortawesome/fontawesome-free-solid/faMobileAlt';
+import faCalendarAlt  from '@fortawesome/fontawesome-free-solid/faCalendarAlt';
 
 export default class FontAwesome {
   static start() {
-    fontawesome.library.add(faSearch);
     fontawesome.library.add(faUpload);
-    fontawesome.library.add(faMinusCircle);
-    fontawesome.library.add(faCheck);
+    fontawesome.library.add(faMarkerAlt);
+    fontawesome.library.add(faUsers);
+    fontawesome.library.add(faSortNumeric);
+    fontawesome.library.add(faMobile);
+    fontawesome.library.add(faCalendarAlt);
   }
 }
