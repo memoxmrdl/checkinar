cb32d0967fab6dd38d699d561ef040db71a75ff8
memoxmrdl <jmemox@gmail.com>
2019-10-02 11:51:59 -0500

    Update secrets by credentials

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,11 +2,11 @@
 
 class JsonWebToken
   def self.encode(payload)
-    JWT.encode(payload, Rails.application.secrets.secret_key_base)
+    JWT.encode(payload, Rails.application.credentials.secret_key_base)
   end
 
   def self.decode(token)
-    HashWithIndifferentAccess.new(JWT.decode(token, Rails.application.secrets.secret_key_base)[0])
+    HashWithIndifferentAccess.new(JWT.decode(token, Rails.application.credentials.secret_key_base)[0])
   rescue
     nil
   end
